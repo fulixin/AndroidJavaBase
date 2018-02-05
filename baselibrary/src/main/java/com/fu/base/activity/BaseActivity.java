469bc611277c3d4cce6dc5f35bb63888a961cf63
fulixin <georgecodyyuxiang@gmail.com>
2018-02-05 14:43:26 +0800

    映射调用类方法

--- baselibrary/src/main/java/com/fu/base/activity/BaseActivity.java
+++ baselibrary/src/main/java/com/fu/base/activity/BaseActivity.java
@@ -1,22 +1,27 @@
 package com.fu.base.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fu.base.annotation.ViewById;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  * Created by fulixin on 2017/8/30.
  */
 
 public abstract class BaseActivity extends AppCompatActivity {
+    private ArrayList<Field> fieldList = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +37,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected abstract void initialzie();
 
     private void initializeLayout() {
-        Field[] fields = getClass().getDeclaredFields();
+        Class<?> clazz = this.getClass();
+        Field[] fields = clazz.getDeclaredFields();// 获得Activity中声明的字段
         for (Field field : fields) {
             if (field.isAnnotationPresent(ViewById.class)) {
                 try {
@@ -42,24 +48,40 @@ public abstract class BaseActivity extends AppCompatActivity {
                     if (viewId > 0) {
                         field.set(this, this.findViewById(viewId));
                     }
+                    if (!viewById.isNull())
+                        fieldList.add(field);
                     if (viewById.onClick()) {
-                        Method method = getClass().getMethod("setOnClickListener", View.OnClickListener.class);
+                        //通过类的.class属性获取
+                        Class<View> clz = View.class;
+                        Method method = clz.getMethod("setOnClickListener", new Class[]{View.OnClickListener.class});
                         method.setAccessible(true);
-                        method.invoke(field.get(this), onclickListener);
+                        method.invoke(this.findViewById(viewId), onclickListener);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-//                try {
-//                } catch (NoSuchMethodException e) {
-//                    e.printStackTrace();
-//                } catch (IllegalAccessException e) {
-//                    e.printStackTrace();
-//                } catch (InvocationTargetException e) {
-//                    e.printStackTrace();
-//                }
             }
         }
+    }
+
+    public boolean isVerify() {
+        for (Field field : fieldList) {
+            try {
+                String str = "";
+                if (((View) (field.get(this))) instanceof TextView) {
+                    str = ((TextView) (field.get(this))).getText().toString();
+                } else if (((View) (field.get(this))) instanceof EditText) {
+                    str = ((EditText) (field.get(this))).getText().toString();
+                }
+                if (str.equals("")) {
+                    Toast.makeText(this, field.getAnnotation(ViewById.class).message(), Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
     }
 
     protected abstract View layoutView();
@@ -69,6 +91,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     public View viewByViewId(int viewId) {
         return LayoutInflater.from(this).inflate(viewId, null);
     }
+
 
     private View.OnClickListener onclickListener = new View.OnClickListener() {
         @Override
--- baselibrary/src/main/java/com/fu/base/annotation/ViewById.java
+++ baselibrary/src/main/java/com/fu/base/annotation/ViewById.java
@@ -1,11 +1,22 @@
 package com.fu.base.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by fulixin on 2017/8/30.
  */
-
+// 表示用在字段上
+@Target(ElementType.FIELD)
+// 表示是在声明周期运行的
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ViewById {
     int viewId() default 0;
 
+    boolean isNull() default true;
+
+    String message() default "";
     boolean onClick() default false;
 }
